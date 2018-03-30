46a475bf5ad3e41b933921ae35dcb70ca64ee10b
DStefanow <dobromirstefanov96@gmail.com>
2018-03-30 10:53:36 +0300

    Split the logic in main file

--- basics/alpha.h
+++ basics/alpha.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "core.c"
 
-char ALPHABET[] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-int LENGTH = sizeof(ALPHABET)/sizeof(char);
+int get_method();
+int compare_strings(char *str1, char *str2);
+int get_arr_position(char symbol);
--- basics/caesar-cipher.c
+++ basics/caesar-cipher.c
@@ -1,10 +1,4 @@
-#include<ctype.h>
-#include <string.h>
 #include "alpha.h"
-
-int get_arr_position(char symbol);
-int get_method();
-int compare_strings(char *str1, char *str2);
 
 void get_user_data();
 void encrypt_data();
@@ -121,68 +115,5 @@ void decrypt_data() {
 	}
 
 	printf("\n");
-}
-
-/**
-* Get method type:
-* 1 - for encrypt
-* 2 - for decrypt
-*/
-int get_method() {
-	char *method;
-	size_t method_max_length = 16;
-	size_t method_length;
-
-	method = (char*)malloc(method_max_length * sizeof(char));
-	// Read the type of the method (encrypt/decrypt)
-	while (1) {
-		printf("Enter method (encrypt/decrypt): ");
-		method_length = getline(&method, &method_max_length, stdin);
-
-		if (compare_strings(method,"encrypt")) {
-			return 1;
-		}
-
-		else if (compare_strings(method, "decrypt")) {
-			return 2;
-		}
-	}
-
-	return 0;
-}
-
-int compare_strings(char *str1, char *str2) {
-	size_t i = 0;
-	size_t len_str1 = strlen(str1) - 1;
-	size_t len_str2 = strlen(str2);
-	size_t max_length = len_str1 > len_str2 ? len_str1 : len_str2; // Remove '\n' on the end of the string
-
-	// Empty string
-	if (len_str1 == 0) {
-		return 0;
-	}
-
-	while (i < max_length) {
-		if (str1[i] != str2[i]) {
-			return 0;
-		}
-
-		i++;
-	}
-
-	return 1;
-}
-
-/**
-	Check if given symbol is in the ALPHABET array
-*/
-int get_arr_position(char symbol) {
-	for(int i = 0; i < LENGTH; i++) {
-		if( tolower(symbol) == ALPHABET[i]) {
-			return i;
-		}
-	}
-
-	return -1;
 }
 
